f3825238cae387928bcceaa46e3cc37b3f09d5a4
陈宇峰 <yufengcode@gmail.com>
2016-08-06 20:00:08 +0800

    最小的k个数

--- LeastKNumber/main.c
+++ LeastKNumber/main.c
@@ -6,10 +6,75 @@
 //  Copyright © 2016 chenyufeng. All rights reserved.
 //
 
+// 数组中最小的k个数
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+void LeastKNumber(int *arr, int start, int end, int k);
+int Partition(int *arr, int start, int end);
+
+int main(int argc, const char * argv[])
+{
+    int array[] = {2,3,4,5,6,7};
+    LeastKNumber(array, 0, 5, 5);
+
     return 0;
+}
+
+void LeastKNumber(int *arr, int start, int end, int k)
+{
+    int index = Partition(arr, start, end);
+
+    while (index != k)
+    {
+        if (index > k)
+        {
+            index = Partition(arr, start, index - 1);
+        }
+        else
+        {
+            // index < k
+            index = Partition(arr, index + 1, end);
+        }
+    }
+
+    if (index == k)
+    {
+        for (int i = 0; i < k; i++)
+        {
+            printf("%d ",arr[i]);
+        }
+        return;
+    }
+}
+
+
+int Partition(int *arr, int start, int end)
+{
+    int i = start, j = end, x = arr[start];
+
+    while (i < j)
+    {
+        while (i < j && arr[j] >= x)
+        {
+            j--;
+        }
+        if (i < j)
+        {
+            arr[i++] = arr[j];
+        }
+
+        while (i < j && arr[i] < x)
+        {
+            i++;
+        }
+        if (i < j)
+        {
+            arr[j--] = arr[i];
+        }
+    }
+
+    arr[i] = x;
+    
+    return i;
 }
